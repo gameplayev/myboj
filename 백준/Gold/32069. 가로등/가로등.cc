3c93361e9f0b02eb0af432b4c04a966e1bedcb64
gameplayev <92735139+gameplayev@users.noreply.github.com>
2024-07-26 00:39:29 +0900

    [Gold V] Title: 가로등, Time: 264 ms, Memory: 28664 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/32069. 가로등/가로등.cc
+++ 백준/Gold/32069. 가로등/가로등.cc
@@ -1,70 +1,63 @@
 #include <iostream>
-#include <algorithm>
+#include <unordered_map>
 #include <vector>
 #include <queue>
-#include <limits.h>
 
 using namespace std;
-
-vector<int> v;
-unsigned long long int l;
-int n, k;
-unsigned long long int limit;
-short dx[2] = {1, -1};
-
-void BFS(int x) {
-    queue<pair<long long int, long long int>> q;
-    q.emplace(x, 0);
-    v[x] = 0; // Start point distance is 0
-    while (!q.empty()) {
-        long long cur = q.front().first;
-        long long cost = q.front().second;
-        q.pop();
-        for (int i = 0; i < 2; i++) {
-            long long cx = dx[i] + cur;
-            if (cx < 0 || cx > limit) continue;
-            if (v[cx] == INT_MAX || v[cx] > cost + 1) {
-                v[cx] = cost + 1;
-                q.emplace(cx, cost + 1);
-            }
-        }
-    }
-}
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cin >> l;
-    cin >> n >> k;
-    
-    unsigned long long iter = min(l, (unsigned long long)n * k);
-    limit = min(l, (unsigned long long)500505);
+    long long l;
+    int n, k;
+    cin >> l >> n >> k;
 
-    // Initialize the vector `v` with size `limit + 1` to handle all possible positions up to `limit`
-    v.resize(limit + 1, INT_MAX); 
+    unordered_map<unsigned long long, int> m;
+    vector<int> res(500050, 0);
+    queue<unsigned long long> q;
+    int limit = 0;
 
     for (int i = 0; i < n; i++) {
-        int a; cin >> a;
-        if (a <= limit) {
-            BFS(a);
+        unsigned long long a;
+        cin >> a;
+        if (m.find(a) == m.end()) {
+            m[a] = 0;
+            q.push(a);
+            res[0]++;
+            limit++;
         }
     }
 
-    // Collect non-INT_MAX values for sorting
-    vector<int> results;
-    for (unsigned long long i = 0; i <= limit; i++) {
-        if (v[i] != INT_MAX) {
-            results.push_back(v[i]);
+    int iter = 1;
+    while (limit < k && !q.empty()) {
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            unsigned long long cur = q.front();
+            q.pop();
+            if (cur > 0 && cur - 1 <= l && m.find(cur - 1) == m.end()) {
+                m[cur - 1] = iter;
+                q.push(cur - 1);
+                res[iter]++;
+                limit++;
+                if (limit >= k) break;
+            }
+            if (cur + 1 <= l && m.find(cur + 1) == m.end()) {
+                m[cur + 1] = iter;
+                q.push(cur + 1);
+                res[iter]++;
+                limit++;
+                if (limit >= k) break;
+            }
         }
+        iter++;
     }
 
-    sort(results.begin(), results.end());
-
-    for (int i = 0; i < k; i++) {
-        if (i < results.size()) {
-            cout << results[i] << "\n";
-        } else {
-            cout << -1 << "\n"; // Output -1 if there are not enough results
+    int count = 0;
+    for (int i = 0; res[i] != 0; i++) {
+        for (int j = 0; j < res[i]; j++) {
+            if (count >= k) break;
+            cout << i << "\n";
+            count++;
         }
     }
 
