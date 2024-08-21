e08cfe3c7a08af5c0be31d4c39fdbcbc3c5181e3
gameplayev <92735139+gameplayev@users.noreply.github.com>
2024-08-22 02:12:39 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> arr;
+vector<int> dp;
+vector<int> x;
+int res = 0, n;
+
+void LIS() {
+    if (!arr.empty()) {
+        x.push_back(arr[0]);
+        dp.push_back(1);
+    }
+
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] > x.back()) {
+            x.emplace_back(arr[i]);
+            dp.push_back(x.size());
+        } else {
+            auto it = lower_bound(x.begin(), x.end(), arr[i]);
+            int idx = it - x.begin();
+            x[idx] = arr[i];
+            dp.push_back(idx + 1);
+        }
+    }
+
+    res = *max_element(dp.begin(), dp.end());  
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a; cin >> a;
+        arr.emplace_back(a);
+    }
+
+    LIS();
+    cout << res << "\n";
+
+    vector<int> lis;
+    int len = res;
+    for (int i = n - 1; i >= 0; i--) {
+        if (dp[i] == len) {
+            lis.push_back(arr[i]);
+            len--;
+        }
+    }
+
+    reverse(lis.begin(), lis.end());
+    for (auto i : lis) {
+        cout << i << ' ';
+    }
+    return 0;
+}
