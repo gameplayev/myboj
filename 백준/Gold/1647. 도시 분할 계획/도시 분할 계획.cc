512517e0beeb0ff8c53255743bba66ab062727f2
gameplayev <92735139+gameplayev@users.noreply.github.com>
2024-08-24 02:07:54 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 296 ms, Memory: 20584 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int,int>;
+
+struct node{
+    int node;
+    int linked_node;
+    int cost;
+};
+
+vector<int> init(vector<int>& par){
+    for(int i = 1;i<par.size();i++){
+        par[i] = i;
+    }
+    return par;
+}
+
+int find(int x,vector<int>& par){
+    if(par[x] == x) return x;
+    return par[x] = find(par[x],par);
+}
+
+void unite(int a,int b, vector<int>& par){
+    a = find(a,par); b = find(b,par);
+    if(a == b) return;
+    par[a] = b;
+}
+
+bool cmp(node & a, node & b){
+    return(a.cost < b.cost);
+}
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n,m; cin>>n>>m;
+    vector<node>graph;
+    for(int i = 0;i<m;i++){
+        node d; int a,b,c; cin>>a>>b>>c;
+        d.node = a; d.linked_node = b; d.cost = c;
+        graph.emplace_back(d);
+    }
+    vector<int>par(n+1);
+    init(par);
+    sort(graph.begin(),graph.end(),cmp);
+    int exp = INT_MIN;
+    long long res = 0;
+    for(int i = 0;i<m;i++){
+        node cur = graph[i];
+        if(find(cur.node,par) == find(cur.linked_node,par)){continue;}
+        unite(cur.node,cur.linked_node,par);
+        exp = max(exp,cur.cost);
+        res += cur.cost;
+    }
+    cout<<res-exp<<"\n";
+    return 0;
+}
