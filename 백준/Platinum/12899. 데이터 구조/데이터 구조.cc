299ad5d907cbf4a2c43f5188e319f306b818cc54
gameplayev <92735139+gameplayev@users.noreply.github.com>
2024-10-15 01:53:30 +0900

    [Platinum IV] Title: 데이터 구조, Time: 932 ms, Memory: 17564 KB -BaekjoonHub

--- 백준/Platinum/12899. 데이터 구조/데이터 구조.cc
+++ 백준/Platinum/12899. 데이터 구조/데이터 구조.cc
@@ -1,48 +1,51 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<stdio.h>
+#define IMAX 2000000
 using namespace std;
 
-void update_elements_in_segment_tree(int node, int start, int end, int index, int buffer, vector<int>& segment_tree) {
-    if(start > index || index > end) return;
-    if (start == end) {
-        segment_tree[node] += buffer;
-        return;
-    }
-    int mid = (start + end) / 2;
-    update_elements_in_segment_tree(node * 2, start, mid, index, buffer, segment_tree);
-    update_elements_in_segment_tree(node * 2 + 1, mid + 1, end, index, buffer, segment_tree);
-    segment_tree[node] = segment_tree[node * 2] + segment_tree[node * 2 + 1];
-}
+class Tree{
+        int node[1<<22];
 
-int find_elements_in_segment_tree(int node, int start, int end, int index, vector<int>& segment_tree) {
-    if (start == end) return start;
-    int mid = (start + end) / 2;
-    if (index <= segment_tree[node * 2]) 
-        return find_elements_in_segment_tree(node * 2, start, mid, index, segment_tree);
-    else 
-        return find_elements_in_segment_tree(node * 2 + 1, mid + 1, end, index - segment_tree[node * 2], segment_tree);
-}
+        public:
+        int Insert(int cur, int left, int right, int index){
+                if(left==right){
+                        node[cur] += 1;
+                        return node[cur];
+                }
 
-int main(void) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n;
-    cin >> n;
-    vector<int> segment_tree(2000001 * 4, 0);
-    int tree_size = 2000001;
-    for (int i = 0; i < n; i++) {
-        int query;
-        cin >> query;
-        int x;
-        cin >> x;
-        if (query == 1) {
-            update_elements_in_segment_tree(1, 1, tree_size, x, 1, segment_tree);
-        } else {
-            int del = find_elements_in_segment_tree(1, 1, tree_size, x, segment_tree);
-            cout << del << "\n";
-            update_elements_in_segment_tree(1, 1, tree_size, del, -1, segment_tree);
+                int mid = (left+right)/2;
+                int ret = 0;
+                if(index <= mid) Insert(cur*2, left, mid, index);
+                if(index > mid) Insert(cur*2+1, mid+1, right, index);
+                return node[cur]= node[cur*2] + node[cur*2+1];
         }
-    }
-    return 0;
+
+        int Delete(int cur, int left, int right, int rank){
+                if(left==right){
+                        node[cur] -= 1;
+                        return left;
+                }
+
+                int mid = (left+right)/2;
+                int ret = 0;
+                if(rank <= node[cur*2]) ret = Delete(cur*2, left, mid, rank);
+
+                else ret = Delete(cur*2+1, mid+1, right, rank-node[cur*2]);
+                node[cur] = node[cur*2] + node[cur*2+1];
+                return ret;
+        }
+};
+int main(){
+        int N;
+        scanf("%d",&N);
+
+        Tree *t = new Tree();
+        for(int i=0;i<N;i++){
+                int q,a;
+                scanf("%d %d",&q,&a);
+                if(q==1){
+                        t->Insert(1, 0, IMAX-1, a-1);
+                }else{
+                        printf("%d\n",t->Delete(1,0,IMAX-1,a)+1);
+                }
+        }
 }
