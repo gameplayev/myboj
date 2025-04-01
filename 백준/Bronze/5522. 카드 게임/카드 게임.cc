fcbe35d687f3353c548215c9fb57afeb3bb88677
gameplayev <92735139+gameplayev@users.noreply.github.com>
2025-04-01 16:28:50 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    long long int total =0;
+    for(int i = 0;i<5;i++){
+        int a; cin>>a;
+        total += a;
+    }
+    cout<<total<<endl;
+    return 0;
+}
